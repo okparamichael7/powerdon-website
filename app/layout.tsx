762550729cefa1a8b55142acd9abfef6ad6086b9
Powerdon <okparamichael7@gmail.com>
2026-03-09 19:07:52 +0100

    Update layout.tsx
    Add Google Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type React from "react";
 import type { Metadata } from "next";
+import Script from "next/script";
 import "@/styles/globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -43,6 +44,15 @@ export default function RootLayout({
       <body className={helveticaFont}>
         <ThemeProvider>{children}</ThemeProvider>
         <Toaster />
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SLDP7D8PQ6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SLDP7D8PQ6');
+</script>
         <Analytics />
       </body>
     </html>
